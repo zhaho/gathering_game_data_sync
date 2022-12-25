b42af2ecf1f338a3b1a57593c47306b9880b4c79
Kenneth Ros <31766495+zhaho@users.noreply.github.com>
2022-12-25 22:30:08 +0100

    Update cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-runuser -l ansible -c '/usr/bin/ansible-pull -o -U https://github.com/zhaho/gameDataToAPI.git' >> /var/log/ansible 2>&1
+runuser -l ansible -c 'ansible-pull -o -U https://github.com/zhaho/gameDataToAPI.git' >> /var/log/ansible 2>&1
